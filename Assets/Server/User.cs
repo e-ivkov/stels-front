6aba7ac14be098bcc414149df506b18e3d08f48f
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-09 14:37:21 +0300

    Minimal client server interface

---
+++ Assets/Server/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Server
+{
+    [Serializable]
+    public class User
+    {
+        public int Id;
+        public string Name;
+        public Vector2 Location;
+        public int Score;
+        public bool Alive;
+        
+        [SerializeField]
+        private string PhotoUrl;
+        private Texture2D _photo = null;
+        
+        private bool _cacheEnabled = true;
+
+        public Texture2D Photo
+        {
+            get
+            {
+                if(_cacheEnabled && _photo != null)
+                    return _photo;
+                using (WWW www = new WWW(PhotoUrl))
+                {
+                    // Wait for download to complete
+                    while(!www.isDone){}
+
+                    // assign texture
+                    _photo = www.texture;
+                    return _photo;
+                }
+            }
+        }
+    }
+}
