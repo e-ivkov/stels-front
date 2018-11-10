f64d350b1c0ae66baa2bfcada5f6630b28466bcb
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-10 15:22:12 +0300

    GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Server;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public IServer Server;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
