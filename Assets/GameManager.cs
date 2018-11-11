dfa3e6258c90e08db54c3675651d8fdd78c77c3e
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-11 14:35:42 +0300

    Basic target selection

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,14 +2,47 @@
 using System.Collections.Generic;
 using Server;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 	public IServer Server;
+	public GameObject IconParent;
+	public GameObject TargetIconPrefab;
+	public bool updateTargets = true;
+	public float UpdateDelay;
+	public GameObject SelectedIcon = null;
 
 	// Use this for initialization
 	void Start () {
 		Server = new Server.Server("test@innopolis.ru", "123qweasd"); //TODO: remove when login screen is ready
+		StartCoroutine(UpdateTargetIcons());
+	}
+
+	IEnumerator UpdateTargetIcons()
+	{
+		while (updateTargets)
+		{
+			foreach (var icon in GameObject.FindGameObjectsWithTag("targetIcon"))
+			{
+				Destroy(icon);
+			}
+			var users = Server.GetUsers();
+			foreach (var user in users)
+			{
+				var icon = Instantiate(TargetIconPrefab, IconParent.transform);
+				icon.GetComponent<TargetIconController>().User = user;
+				icon.GetComponent<TargetIconController>().GameManager = gameObject.GetComponent<GameManager>();
+				var tex = user.Photo;
+				var sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+				icon.GetComponent<Image>().sprite = sprite;
+				if (SelectedIcon == null)
+				{
+					icon.GetComponent<TargetIconController>().Select();
+				}
+			}
+			yield return new WaitForSeconds(UpdateDelay);
+		}
 	}
 	
 	// Update is called once per frame
